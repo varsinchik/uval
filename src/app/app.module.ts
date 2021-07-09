e5a652688c7769c4529e3d2f26bc5c5f5eee1044
varsinchik <redmiya@yandex.ru>
2021-07-09 15:07:26 +0300

    Созданмодуль homecomponent-а

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,22 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {ForBeerComponent} from './home/for-beer/for-beer.component';
-import {ExpensesComponent} from './home/expenses/expenses.component';
-import {BudgetComponent} from './home/budget/budget.component';
 import {HomeComponent} from './home/home.component';
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ForBeerComponent,
-    ExpensesComponent,
-    BudgetComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HomeModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
